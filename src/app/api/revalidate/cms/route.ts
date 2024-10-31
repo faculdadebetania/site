4255f16b89d4fa56e290e25908a50c19bbed6f05
Kauê Delmonte <kaue@delmonte.dev>
2024-10-30 23:50:22 -0300

    fix(cache): changes route handler path

--- src/app/api/revalidate/cms/route.ts
+++ src/app/api/revalidate/cms/route.ts
@@ -1,15 +1,14 @@
 import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request, { params }: { params: Promise<{ tag: string }> }) {
+export async function POST(request: Request) {
   try {
     let { REVALIDATE_TOKEN } = process.env;
     const requestHeaders = new Headers(request.headers);
     const token = requestHeaders.get('Revalidate-Token');
     if (!token) return NextResponse.json({ message: 'Missing Revalidate-Token Header' }, { status: 403 });
     if (REVALIDATE_TOKEN !== token) return NextResponse.json({ message: 'Invalid Token' }, { status: 401 });
-    const { tag } = await params;
-    revalidateTag(tag);
+    revalidateTag('cms');
     return NextResponse.json({ message: 'Success' });
   } catch (error) {
     const message = (error as Error).message ?? error;
